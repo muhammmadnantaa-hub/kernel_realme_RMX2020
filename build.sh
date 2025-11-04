7be0d486a8b8e426d51ab73b8f2200a1b996f661
M.Rody Mananta <muhammmadnantaa@gmail.com>
2025-11-04 07:14:12 +0000

    build

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
 KERNEL_NAME="SukiSuA16${BUILD_TYPE}-${DATE}.zip"
 
 # Clone SukiSU repo
-if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/kernel-build-from-rainyland/KernelSU-Next/next/kernel/setup.sh" | bash -s next fi
+if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/kernel-build-from-rainyland/KernelSU-Next/next/kernel/setup.sh" | bash -s next; fi
 
 function KERNEL_COMPILE() {
 	if [ "$1" == "install" ]; then
